13dc31b3f72c04f12c45310ab4cdbf59f203f368
ils-juhis <j.sahu@thesynapses.com>
2023-07-29 16:51:05 +0530

    useRef  hook

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
     <div className="App">
       {/* <StateTutorial /> */}
       {/* <EffectTutorial/> */}
-      <ContextTutorial/>
+      {/* <ContextTutorial/> */}
+      <RefTutorial/>
     </div>
   );
 }
--- src/UseRef/RefTutorial.js
+++ src/UseRef/RefTutorial.js
@@ -3,13 +3,17 @@ import React, { useRef } from "react";
 function RefTutorial() {
   const inputRef = useRef(null);
 
+  const changeHandler = (e)=>{
+    inputRef.current.value = e.target.value;
+  }
+
   const onClick = () => {
     inputRef.current.value = "";
   };
   return (
     <div>
       <h1>Pedro</h1>
-      <input type="text" placeholder="Ex..." ref={inputRef} />
+      <input type="text" placeholder="Ex..." ref={inputRef} onChange={changeHandler}/>
       <button onClick={onClick}>Change Name</button>
     </div>
   );
